a6919eed0c6ebc7ec4817cd93e46b05b7cb64709
Tarush Mohindru <tarushmohindru1515@gmail.com>
2025-10-17 11:40:39 +0000

    Implemented admin routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { connectDataBase } from './config/db.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import userRoutes from './routes/user.routes.js'
+import adminRoutes from './routes/admin.routes.js'
 
 dotenv.config()
 
@@ -26,6 +27,7 @@ app.get('/api', (req, res) => {
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/admin', adminRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
